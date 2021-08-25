b3e6df448a576f3c46ae42dbf53924adcc0277a0
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-08-25 12:38:45 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+echo ">> starting configuration for minio"
+
 mc --config-dir=/opt/.mc config host add source_acct https://s3."$SOURCE_REGION".cloud-object-storage.appdomain.cloud ${SOURCE_ACCESS_KEY} ${SOURCE_SECRET_KEY} --api "s3v4"
 
 mc --config-dir=/opt/.mc config host add destination_acct https://s3."${DESTINATION_REGION}".cloud-object-storage.appdomain.cloud ${DESTINATION_ACCESS_KEY} ${DESTINATION_SECRET_KEY} --api "s3v4"
